4feae23762ca0a2ac43a57f134cbe553044d3dde
sidneiandre <97473368+sidneiandre@users.noreply.github.com>
2024-12-11 13:47:09 -0300

    adicionando controles de volumes

--- src/styles/engine.js
+++ src/styles/engine.js
@@ -1,7 +1,28 @@
 const pianokeys = document.querySelectorAll(".pianokeys .key");
 
+let mapedKeys = [];
+let audio = new Audio("src/tunes/a.wav");
 
-pianokeys.forEach((key)=>{
-    console.log(key.dataset.key);
+const playTuner = (key) => {
+    audio.src = `src/tunes/${key}.wav`;
+    audio.play();
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add("active");
+    setTimeout(() => {
+        clickedKey.classList.remove("active");
+    }, 150);
+};
+
+
+pianoKeys.forEach((key)=>{
     key.addEventListener("click",() => playTuner(key.dataset.key));
+    mapedKeys.push(key.dataset.key);
+});
+
+document.addEventListener("keydown", (e) => {
+
+    if(mapedKeys.includes(e.key)){
+        playTuner(e.key);
+    }   
 });
